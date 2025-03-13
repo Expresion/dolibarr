c7f4bbbcfcb8da964e9a09649389cdadfd8e4ed0
Expresion <info@expresion.net>
2025-03-13 19:57:24 +0100

    Update document.php
    Fix hook in facture/document

--- htdocs/compta/facture/document.php
+++ htdocs/compta/facture/document.php
@@ -100,6 +100,15 @@ $usercancreate = $user->hasRight("facture", "creer");
 include DOL_DOCUMENT_ROOT.'/core/actions_linkedfiles.inc.php';
 
 
+$reshook = $hookmanager->executeHooks('doActions', array(), $object, $action); // Note that $action and $object may have been modified by some hooks
+if ($reshook < 0) {
+	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
+}
+if (empty($reshook)) {
+	include DOL_DOCUMENT_ROOT.'/core/actions_linkedfiles.inc.php';
+}
+
+
 /*
  * View
  */
