07337519fc3e59c4f593cd9e3289ac3e442bb227
Expresion <info@expresion.net>
2025-04-15 22:16:03 +0200

    Revert "Update card.php"
    This reverts commit 733460edc47554b79f993e41bc7cef0136c68242.

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -861,10 +861,6 @@ if (empty($reshook)) {
 			if ($result < 0) {
 				setEventMessages($object->error, $object->errors, 'errors');
 			}
-			if ($result >= 0) {
-				header("Location: ".$_SERVER['PHP_SELF']."?id=".$id);
-				exit;
-			}
 		} else {
 			setEventMessages($langs->trans("ErrorFieldRequired", $langs->transnoentitiesnoconv("Reason")), null, 'errors');
 		}
